b0a54b5fd1d8673517e0f313234c568cb150f1cc
BieFeNg <feng.bie@myt11.com>
2019-05-09 21:34:24 +0800

    Initial commit

---
+++ src/com/biefeng/App.java
@@ -0,0 +1,12 @@
+package com.biefeng;
+
+/*
+ *@Author BieFeNg
+ *@Date 2019/5/9 21:33
+ *@DESC
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
